fa93e610251365accf92161d63a2a248683da30d
CarolinaHoffer <damihoylin@gmail.com>
2020-12-16 17:55:14 -0300

    se agrego cambios al app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 //Components
-import { ItemComponent } from './sidebar/item/item.component';
-import { NavComponent } from './sidebar/nav/nav.component';
-
+import { ItemComponent } from './components/sidebar/item/item.component';
+import { NavComponent } from './components/sidebar/nav/nav.component';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 //Material
-import { MatListModule } from "@angular/material/list";
+
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
+
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    ItemComponent
+    ItemComponent,
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatSidenavModule,
-    MatListModule,
     MatButtonModule,
     MatIconModule,
   ],
