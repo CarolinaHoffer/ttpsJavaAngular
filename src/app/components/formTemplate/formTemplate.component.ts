21746129f94adcf9c436f6d9a885e4517fd0c043
Dolores Garcia <dgarcia@linti.unlp.edu.ar>
2021-02-17 21:02:24 -0300

    Se unificaron los componentes nav e items en uno solo: navbar. Se creo componente template que muestra a la izquierda el menu y a la derecha un router-outlet.

---
+++ src/app/components/formTemplate/formTemplate.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-template',
+  templateUrl: './formTemplate.component.html',
+  styleUrls: ['./formTemplate.component.css'],
+})
+export class FormTemplateComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
--- src/app/components/home/navbar/navbar.component.spec.ts
+++ src/app/components/home/navbar/navbar.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { TemplateComponent } from './template.component';
+import { NavbarComponent } from './navbar.component';
 
-describe('TemplateComponent', () => {
-  let component: TemplateComponent;
-  let fixture: ComponentFixture<TemplateComponent>;
+describe('NavbarComponent', () => {
+  let component: NavbarComponent;
+  let fixture: ComponentFixture<NavbarComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ TemplateComponent ]
+      declarations: [ NavbarComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(TemplateComponent);
+    fixture = TestBed.createComponent(NavbarComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
